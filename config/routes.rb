9be5d4589313035ee0b0820d667aa563805860ff
Andrew Nash <andrew.nash@gmail.com>
2012-08-06 10:53:32 -0700

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "static_pages/home"
 
-  get "pages/contact"
+  get "static_pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
